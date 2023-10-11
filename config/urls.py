4ad74a6701350042bb2993f7280f8f5d07068ba4
Rasul <rasulabdikerimov85@gmail.com>
2023-10-11 10:55:59 +0500

    authentication is done

--- config/urls.py
+++ config/urls.py
@@ -4,9 +4,12 @@ from django.urls import path
 from todo import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.home, name='home'),
 
     #auth
-    path('signup/',views.signupuser, name='signupuser')
+    path('signup/',views.signupuser, name='signupuser'),
+    path('login/',views.loginuser, name='loginuser'),
+    path('logout/',views.logoutuser, name='logoutuser'),
 
 
     #todos
--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
+from django.contrib.auth import login,logout,authenticate
+
+
 
 def signupuser(request):
     if request.method == 'GET':
@@ -11,6 +14,8 @@ def signupuser(request):
             try:
                 user = User.objects.create_user(request.POST['username'],password=request.POST['password1'])
                 user.save()
+                login(request,user)
+                return redirect('home')
             except IntegrityError:
                     return render(request,'todo/signupuser.html',
                    {'form':UserCreationForm() ,
@@ -18,6 +23,24 @@ def signupuser(request):
 
         else:
             #qollaniwshiga paroller tuwri kemewin aytiw kerek
-           return render(request,'todo/signupuser.html',{'form':UserCreationForm(),'error':'Paroller tuwri kelmeydi',}) 
-            
+           return render(request,'todo/signupuser.html',{'form':UserCreationForm(),'error':'Paroller tuwri kelmeydi',})
+def loginuser(request):
+    if request.method == 'GET':
+        return render(request,'todo/loginuser.html',{'form':AuthenticationForm()})
+    else:
+        user=authenticate(request,username=request.POST['username'],password=request.POST['password'])
+        if user is None:
+            return render(request,'todo/loginuser.html',{'form':AuthenticationForm(),'error':'Username and password did not match'})
+        else:
+            login(request,user)
+            return redirect('home')
+
+def logoutuser(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('home')
+
+     
+def home(request):
+    return render(request,'todo/home.html')       
     
